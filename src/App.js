bbf6d3fbe60f199ac65e5f3c93560af005815620
darren2581 <darrentanthongen@gmail.com>
2024-11-11 18:09:11 +0800

    UI done

--- src/App.js
+++ src/App.js
@@ -7,25 +7,45 @@ function App() {
     <div className="container">
       <h2 className='title'>Expenses Tracker</h2>
 
+      {/* Overall Transaction Details*/}
         <div className='overall-details'>
         <div className='budjet'>
-          <h4>Income</h4>
-          <h5>RM100.00</h5>
+          <h4>Budjet</h4>
+          <p className='detail-amount'>RM100.00</p>
         </div>
         <div className='remaining'>
           <h4>Balance</h4>
-          <h5>RM70.00</h5>
+          <p className='detail-amount'>RM100.00</p>
         </div>
         <div className='expenses'>
           <h4>Expenses</h4>
-          <h5>RM30.00</h5>
+          <p className='detail-amount'>RM100.00</p>
         </div>
       </div>
 
+      {/* Add Transaction */}
+      <div className='search-bar'>
+        <h3 className='title-new-expenses'>Add New Expenses</h3>
+        <form className='add-transaction'>
+          <input type='text' 
+            className='input-name' 
+            placeholder='Enter Transaction Name'
+          />
+          <input type='text' 
+            className='input-details' 
+            placeholder='Enter Transaction Details'
+          />
+        </form>
+        <button type="submit" className='add-btn'>
+          <p>Add expenses</p>
+        </button>
+      </div>
+
+      {/* Show Transactions */}
       <div className='transaction-list'>
         <div>
           <h3 className='title-expenses'>Expenses</h3>
-          <ul>
+          <ul className='scrollable-list'>
             <li className='expenses-item'>
               <span className='name'>House loan </span>
               <span className='amount'>RM100.00</span>
